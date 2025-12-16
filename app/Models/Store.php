44fa248a434a144f49f2053eb34ef1804858b5c3
OthmaneDevLab <adamlamzaouak987@gmail.com>
2025-12-16 17:13:12 +0100

    feat: add authentication, roles, vendors and stores

---
+++ app/Models/Store.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    use HasFactory;
+     protected $fillable = [
+        'vendor_id',
+        'name',
+        'slug',
+        'description',
+    ];
+    public function vendor()
+{
+    return $this->belongsTo(Vendor::class);
+}
+}
